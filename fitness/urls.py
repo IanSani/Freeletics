e9643be72257364a82745f59b0db0b1ed2397155
IanSani <iansani259@gmail.com>
2019-06-06 12:15:30 +0300

    Added urls

--- fitness/urls.py
+++ fitness/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import views
+from django.conf.urls import include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'',include('freeletics.urls'))
 ]
--- freeletics/views.py
+++ freeletics/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
 
 # Create your views here.
+def home(request):
+    if request.user.is_authenticated:
+        return render(request, 'index.html',)
